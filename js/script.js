a53b6afa4ae03abd920fc91be00ac14f66a3240c
MichaelC2 <Michaelgcoker2@gmail.com>
2019-11-13 23:18:44 +0000

    Rock pappers scissors project

--- js/script.js
+++ js/script.js
@@ -12,4 +12,29 @@
 $("button").click(function() {
     let choice = $("#input").val()
     $("#userChoice").html(choice)
+    let randomNumber = Math.random();
+    randomNumber = randomNumber * 10
+    let comChoice = computerChoice(randomNumber)
+    $("#computerChoice").html(comChoice)
 })
+
+function computerChoice(number) {
+  if(number >= 6) {
+    return "rock"
+  } 
+  if (number < 6 && number > 3) {
+    return "paper"
+  } else if (number <= 3) {
+    return "scissors"
+  } 
+  
+} 
+
+function theResults() {
+    if ()
+}
+
+
+
+let randomVar = Math.random();
+randomVar = randomVar * 10;
